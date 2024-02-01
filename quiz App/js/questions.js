923b6f5ac4f73333c4cfb4a474dec41a66d61cba
sumit <sumitsaini576@gmail.com>
2024-02-01 17:57:26 +0530

    .

---
+++ quiz App/js/questions.js
@@ -0,0 +1,168 @@
+// creating an array and passing the number, questions, options, and answers
+// easy
+let questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language",
+    ],
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet",
+    ],
+  },
+  {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor",
+    ],
+  },
+  {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language",
+    ],
+  },
+  {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language",
+    ],
+  },
+];
+
+// medium
+let questionsMedium = [
+  {
+    numb: 1,
+    question: "What is the purpose of the 'git clone' command?",
+    answer: "To copy a repository from a remote to local machine",
+    options: [
+      "To create a new repository",
+      "To delete a repository",
+      "To merge branches in a repository",
+      "To copy a repository from a remote to local machine",
+    ],
+  },
+  {
+    numb: 2,
+    question: "What is the box model in CSS?",
+    answer: "It consists of content, padding, border, and margin",
+    options: [
+      "It is a model for creating 3D transformations",
+      "It represents the structure of an HTML document",
+      "It consists of content, padding, border, and margin",
+      "It is a layout design pattern",
+    ],
+  },
+  {
+    numb: 3,
+    question: "In JavaScript, what is the 'this' keyword?",
+    answer: "It refers to the object it belongs to",
+    options: [
+      "It is used to declare variables",
+      "It refers to the global object",
+      "It refers to the parent function",
+      "It refers to the object it belongs to",
+    ],
+  },
+  {
+    numb: 4,
+    question: "What is the purpose of the 'npm' command in Node.js?",
+    answer: "To manage packages and dependencies in a Node.js project",
+    options: [
+      "To create a new Node.js project",
+      "To run JavaScript code in the browser",
+      "To install a new programming language",
+      "To manage packages and dependencies in a Node.js project",
+    ],
+  },
+  {
+    numb: 5,
+    question:
+      "What is the difference between 'let', 'const', and 'var' in JavaScript?",
+    answer: "They are used to declare variables with different scopes",
+    options: [
+      "They are different syntaxes for the same thing",
+      "They represent different data types",
+      "They are used for mathematical operations",
+      "They are used to declare variables with different scopes",
+    ],
+  },
+];
+
+// hard
+let questionsHard = [
+  {
+    numb: 1,
+    question:
+      "Which of the following number object function returns the value of the number?",
+    answer: "valueOf()",
+    options: ["toString()", "valueOf()", "toLocaleString()", "toPrecision()"],
+  },
+  {
+    numb: 2,
+    question:
+      "Which of the following function of the String object returns the character in the string starting at the specified position via the specified number of characters?",
+    answer: "substr()",
+    options: ["slice()", "split()", "substr()", "search()"],
+  },
+  {
+    numb: 3,
+    question:
+      "Explain the purpose of the 'map', 'reduce', and 'filter' functions in JavaScript.",
+    answer:
+      "They are higher-order functions used for manipulating arrays and processing data.",
+    options: [
+      "They are used for creating graphical elements in web development.",
+      "They are exclusive to server-side programming.",
+      "They are used for defining custom data types.",
+      "They are higher-order functions used for manipulating arrays and processing data.",
+    ],
+  },
+  {
+    numb: 4,
+    question: " In JavaScript the x===y statement implies that:",
+    answer: "Both are equal in the value and data type.",
+    options: [
+      "Both x and y are equal in value, type and reference address as well.",
+      "Both are x and y are equal in value only.",
+      "Both are equal in the value and data type.",
+      "Both are not same at all.",
+    ],
+  },
+  {
+    numb: 5,
+    question:
+      " Choose the correct snippet from the following to check if the variable 'a' is not equal the 'NULL':",
+    answer: "if(a!==null)",
+    options: ["if(a!==null)", "if (a!)", "if(a!null)", "if(a!=null)"],
+  },
+];
